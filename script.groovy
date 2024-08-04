ecae06db5751d623ec44a062bee3b5d370ce6324
Ghanem <ahmed_ghanem2564@yahoo.com~>
2024-08-04 23:51:49 +0300

    update groovy

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-    sh 'docker build -t ghanemovic/jenkins-task:jm-2.0 .'
+    sh 'docker build -t ghanemovic/jenkins-task:jm-3.0 .'
     sh "echo $PASS | docker login -u $USER --password-stdin"
-    sh 'docker push ghanemovic/jenkins-task:jm-2.0'
+    sh 'docker push ghanemovic/jenkins-task:jm-3.0'
     }
 } 
 
